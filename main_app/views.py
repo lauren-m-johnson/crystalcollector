2e1752aabd71eac59b5e0025fbbc01010cebc6da
lauren-m-johnson <laurenmj90@gmail.com>
2023-07-25 15:54:06 -0700

    complete lab

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
+crystals = [
+    {'name': 'Quartz', 'hardness': 7, 'structure': 'hexagonal'},
+    {'name': 'Calcite', 'hardness': 3, 'structure': 'trigonal'},
+    {'name': 'Halite', 'hardness': 2.5, 'structure': 'cubic'},
+    {'name': 'Fluorite', 'hardness': 4, 'structure': 'cubic'},
+    {'name': 'Diamond', 'hardness': 10, 'structure': 'cubic'},
+]
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def crystals_index(request):
+    return render(request, 'crystals/index.html', {
+        'crystals': crystals
+    })
